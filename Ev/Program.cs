2d3c71eb77ac3300259f4b633d67e314e66d8019
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-29 00:56:43 +0300

    21

---
+++ Ev/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sual_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            int sum;
+            Console.Write("Birinci ededinizi daxil edin: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ikinci  ededinizi daxil edin: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            sum = number1 + number2;
+            Console.WriteLine("Toplam:" + sum);
+        }
+    }
+}
